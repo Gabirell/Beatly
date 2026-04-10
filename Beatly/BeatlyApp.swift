78882cf50a059d262286eeffbe73b42ec9b12722
Gabriel Netto <gabriel.netto@gmail.com>
2026-04-10 15:06:31 -0300

    Initial Commit

---
+++ Beatly/BeatlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatlyApp.swift
+//  Beatly
+//
+//  Created by Gabriel Netto on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BeatlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beatly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beatly
+//
+//  Created by Gabriel Netto on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
